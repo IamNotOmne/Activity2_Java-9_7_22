6eaa3e1089179768200fd19206b9f39fcd3a1e7c
RJhaine <21103788@usc.edu.ph>
2022-09-12 15:27:00 +0800

    Added changes to the subtraction

--- Subtraction.java
+++ Subtraction.java
@@ -1,24 +1,16 @@
-class Main {
-  public static void main(String[] args) {
+import java.util.Scanner;
 
-    char operator;
-    Double number1, number2, result;
-
-    Scanner input = new Scanner(System.in);
-
-    // ask users to enter numbers
-    System.out.println("Enter first number");
-    number1 = input.nextDouble();
-
-    System.out.println("Enter second number");
-    number2 = input.nextDouble();
-
-     
-      case '-':
-        result = number1 - number2;
-        System.out.println(number1 + " - " + number2 + " = " + result);
-        break;
-
-    input.close();
-  }
+public class Subtraction extends Calculation {
+     void result(){
+        Scanner difference = new Scanner(System.in);
+        
+        System.out.print("1st Number: ");
+        int x = difference.nextInt();
+        
+        System.out.print("2nd Number: ");
+        int y = difference.nextInt();
+        
+        int subtract = x - y;
+        System.out.println("Result is "  + subtract);
+    }
 }
